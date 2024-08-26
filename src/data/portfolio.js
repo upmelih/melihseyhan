8cd27e3997a44c8d12b620307c4667407d18d530
melih <melihseyhan@gmail.com>
2024-08-26 11:51:04 -0600

    minimalistic portfolia websites

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+   {
+      title: 'Smart Waste App',
+      imgUrl: '/assets/terminal-website.jpg',
+      stack: ['React.js', 'Node.js', 'InfluxDb', 'Vite'],
+      //link: 'https://fkcodes.com',
+   },
+   {
+      title: 'School Managment App',
+      imgUrl: '/assets/dev-portfolio.jpg',
+      stack: ['React', 'ASP.NET', 'C#'],
+      //link: 'https://thehackershideout.com',
+   },
+   {
+      title: 'AI Nerf Gun',
+      imgUrl: '/assets/ai-nerf-gun.jpg',
+      stack: ['Python', 'C++', 'Robotics'],
+      //link: 'https://youtu.be/A8eegxL82PI',
+   },
+]
