0ea8b52581d79a19307192fed2521e16009b181b
melih <melihseyhan@gmail.com>
2024-08-26 11:51:52 -0600

    change the time line

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -15,21 +15,21 @@ export default [
 	},
 	{
 		year: '2019',
-		title: 'Software Engineer',
+		title: 'Moving the Canda',
 		duration: '2 years',
 		details:
 			'I helped build an enterprise application for a Fortune 500 company in an Agile team setting. Our tech stack consisted of TypeSciprt, Angular, Java, Gradle, and a handful of other languages and tools.',
 	},
 	{
-		year: '2018',
-		title: "Bachelor's Degree in Computer Science",
+		year: '2017',
+		title: "First Line of Code",
 		duration: '5 years',
 		details:
 			'I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.',
 	},
 	{
-		year: '2013',
-		title: 'First Line of Code',
+		year: '2012',
+		title: "Bachelor's Degree in Economics",
 		duration: 'the beginning',
 		details:
 			"I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
