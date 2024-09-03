9a03eb25011499540b2bef271cc7eb18c54179b8
melih <melihseyhan@gmail.com>
2024-09-03 10:21:58 -0600

    adding to url to make contact form functional

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
       <div className="flex flex-col mb-10 mx-auto">
          <div className="flex justify-center items-center">
             <form
-               action="https://getform.io/f/(customSlugHere)"
+               action="https://getform.io/f/adrylvma"
                method="POST"
                className="flex flex-col w-full md:w-7/12"
             >
